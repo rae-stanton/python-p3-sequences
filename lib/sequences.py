412b94646a26d9d0773956eb05986b1a855fd8b3
Rae Stanton <raestanton93@gmail.com>
2023-09-11 14:35:02 -0700

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length < 0:
+        print("Input should be a non-negative integer")
+        return
+
+    fibonacci_seq = []
+
+    if length > 0:
+        fibonacci_seq.append(0)
+    if length > 1:
+        fibonacci_seq.append(1)
+
+    for i in range(2, length):
+        next_value = fibonacci_seq[-1] + fibonacci_seq[-2]
+        fibonacci_seq.append(next_value)
+
+    print(fibonacci_seq)
     pass
